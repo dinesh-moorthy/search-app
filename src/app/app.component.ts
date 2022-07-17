9d300469b661140abf7ccb7a32cc324414c438f7
dinesh-moorthy <dineshmoorthy9595@gmail.com>
2022-07-17 14:20:51 +0530

    fix: cookie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.cookie = 'name=search-app';
+  }
 }
